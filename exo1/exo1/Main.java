71612795b63fea1bb6fde420bc3f861f5c115199
Elias91120 <164196947+Elias91120@users.noreply.github.com>
2025-03-11 15:53:08 +0100

    Exercice 1 : Utilisation du constructeur et amélioration des affichages dans Main.java
    Modification de l'instanciation de Voiture pour utiliser le nouveau constructeur.
    Ajout de l'affichage des attributs de la voiture avant d'exécuter ses méthodes.
    Code réorganisé pour améliorer la lisibilité.

--- exo1/exo1/Main.java
+++ exo1/exo1/Main.java
@@ -1,12 +1,19 @@
 package exo1;
 
+// Classe Main pour tester la classe Voiture
 public class Main {
     public static void main(String[] args) {
-        Voiture voiture = new Voiture();
-        voiture.marque = "Toyota";
-        voiture.modele = "Corolla";
-        voiture.couleur = "Rouge";
+        // Création d'une voiture avec le constructeur
+        Voiture voiture = new Voiture("Toyota", "Corolla", "Rouge");
 
+        // Affichage des informations de la voiture
+        System.out.println("Marque : " + voiture.getMarque());
+        System.out.println("Modèle : " + voiture.getModele());
+        System.out.println("Couleur : " + voiture.getCouleur());
+
+        // Test des méthodes
         voiture.demarrer();
+        voiture.accelerer();
+        voiture.freiner();
     }
 }
