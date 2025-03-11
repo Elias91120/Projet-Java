e5eba8de168edaa91a671343a51a9b55dee7f7aa
Elias91120 <164196947+Elias91120@users.noreply.github.com>
2025-03-11 15:51:57 +0100

    Exercice 1 : Encapsulation des attributs et ajout d'un constructeur dans Voiture.java
    Les attributs marque, modele et couleur sont maintenant private (encapsulation).
    Ajout d'un constructeur pour initialiser les valeurs dès la création d'un objet Voiture.
    Ajout de getters et setters pour accéder/modifier les attributs de manière sécurisée.
    Amélioration des méthodes demarrer(), accelerer(), et freiner() pour afficher des messages plus détaillés.

--- exo1/exo1/Voiture.java
+++ exo1/exo1/Voiture.java
@@ -1,24 +1,54 @@
 package exo1;
 
-// Voiture.java
+// Classe Voiture améliorée avec encapsulation
 public class Voiture {
-    // Attributs
-    public String marque;
-    public String modele;
-    public String couleur;
+    // Attributs privés
+    private String marque;
+    private String modele;
+    private String couleur;
 
-    public Voiture() {}
+    // Constructeur
+    public Voiture(String marque, String modele, String couleur) {
+        this.marque = marque;
+        this.modele = modele;
+        this.couleur = couleur;
+    }
+
+    // Getters et Setters
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getCouleur() {
+        return couleur;
+    }
+
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
 
     // Méthodes
     public void demarrer() {
-        System.out.println("La voiture démarre");
+        System.out.println("La " + marque + " " + modele + " de couleur " + couleur + " démarre.");
     }
-	
-	public void accelerer() {
-        System.out.println("La voiture accelère");
+
+    public void accelerer() {
+        System.out.println("La " + marque + " " + modele + " accélère.");
     }
 
     public void freiner() {
-        System.out.println("La voiture freine");
+        System.out.println("La " + marque + " " + modele + " freine.");
     }
 }
